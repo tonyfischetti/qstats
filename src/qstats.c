54df65edefe30acef2960b53ba6e41b9ad8de74e
Tony Fischetti <tony.fischetti@gmail.com>
2013-10-08 16:18:22 -0400

    added MIT copyright to the source files

--- src/qstats.c
+++ src/qstats.c
@@ -1,3 +1,28 @@
+
+/* Copyright (c) 2013, Tony Fischetti
+ *
+ * MIT License, http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -207,7 +232,7 @@ int main(int argc, char **argv){
                     char *res;
                     FREQ_BREAKS = strtol(optarg, &res, 10);
                     if(*res){
-                        fputs("Can't parse frequency breaks, expects integer\n",
+                        fputs("Can't parse breaks, expects integer\n",
                               stderr);
                         exit(EXIT_FAILURE);
                     }
@@ -223,7 +248,7 @@ int main(int argc, char **argv){
                     char *res2;
                     FREQ_BREAKS = strtol(optarg, &res2, 10);
                     if(*res2){
-                        fputs("Can't parse barchart breaks, expects integer\n",
+                        fputs("Can't barchart breaks, expects integer\n",
                               stderr);
                         exit(EXIT_FAILURE);
                     }
